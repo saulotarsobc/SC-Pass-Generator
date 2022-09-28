659d2400c9650e25500307e1899da6d43928c139
Saulo Costa <saulotarsobc@gmail.com>
2022-09-28 09:23:31 -0300

    :fire:

--- main.js
+++ main.js
@@ -2,28 +2,27 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 390,
-    height: 600,
-    maxWidth: 390,
-    maxHeight: 600,
-    minWidth: 390,
-    minHeight: 600,
-  });
+    const mainWindow = new BrowserWindow({
+        width: 390,
+        height: 600,
+        maxWidth: 390,
+        maxHeight: 600,
+        minWidth: 390,
+        minHeight: 600,
+    });
 
-  mainWindow.loadFile('index.html');
-  mainWindow.setMenu(null);
-  // mainWindow.webContents.openDevTools()
+    mainWindow.loadFile('index.html');
+    mainWindow.setMenu(null);
+    // mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    createWindow();
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
 });
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit();
 });
